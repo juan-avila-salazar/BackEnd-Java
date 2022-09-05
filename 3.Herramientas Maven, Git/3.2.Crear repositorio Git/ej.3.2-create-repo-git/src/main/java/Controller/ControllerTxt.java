1c19f9046827363d130c8a7963ae6931fb38e27a
Juan Pablo Avila Salazar <juanpablo.avila@bosonit.com>
2022-09-05 18:01:14 +0200

    prueba primera linea de fichero

---
+++ 3.Herramientas Maven, Git/3.2.Crear repositorio Git/ej.3.2-create-repo-git/src/main/java/Controller/ControllerTxt.java
@@ -0,0 +1,29 @@
+package Controller;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Scanner;
+
+public class ControllerTxt {
+    public void AddContentToTxt1 (){
+        Path path = Paths.get("C:\\Users\\juanpablo.avila\\Desktop\\Proyectos Instrucion\\BackEnd Java\\3.Herramientas Maven, Git\\3.2.Crear repositorio Git\\Ficheros\\fichero1.txt");
+        String add, res;
+        Scanner sc = new Scanner(System.in);
+        try{
+            System.out.println("Do you want to add content to the file? y/n");
+            res = sc.nextLine();
+            while (res.equalsIgnoreCase("y")){
+                add = sc.nextLine();
+                add=add+"\n";
+                Files.write(path,add.getBytes(), StandardOpenOption.APPEND);
+                System.out.println("Do you want to continue adding content? y/n");
+                res=sc.nextLine();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- 3.Herramientas Maven, Git/3.2.Crear repositorio Git/ej.3.2-create-repo-git/src/main/java/Greeting/HelloWorld.java
+++ 3.Herramientas Maven, Git/3.2.Crear repositorio Git/ej.3.2-create-repo-git/src/main/java/Greeting/HelloWorld.java
@@ -1,8 +1,13 @@
 package Greeting;
 
+import Controller.ControllerTxt;
+
 public class HelloWorld {
     public static void main(String[] args) {
+        ControllerTxt obj = new ControllerTxt();
 
         System.out.println("Hola mundo");
+
+        obj.AddContentToTxt1();
     }
 }
