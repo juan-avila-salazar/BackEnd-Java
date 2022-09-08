3ca801464e1cc9eb5deffb395882587ea7028656
juan-avila-salazar <juanpablo.avila@bosonit.com>
2022-09-08 16:16:40 +0200

    creacion de punto 5.1 /2

---
+++ 5.Spring Boot(Basico)/5.1.Servidor Web/ServidorWeb/src/test/java/com/webHola/ServidorWeb/ServidorWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webHola.ServidorWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
