2d0f4da8a5d49205c32b68929d9897fe635ac12e
juan-avila-salazar <juanpablo.avila@bosonit.com>
2022-09-14 15:35:52 +0200

    5.1 nuevo

--- 5.Spring Boot(Basico)/5.1.ComandLineRuner/ej5.1-command-line-runner/src/main/java/com/ej5/commandlinerunner/Application.java
+++ 5.Spring Boot(Basico)/5.1.ComandLineRuner/ej5.1-command-line-runner/src/main/java/com/ej5/commandlinerunner/Application.java
@@ -1,13 +1,24 @@
 package com.ej5.commandlinerunner;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
-public class Application {
+@Slf4j
+public class Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
+		//log.info("Hola desde la clase Inicial.");
 		SpringApplication.run(Application.class, args);
+		log.info("Soy la tercera clase ");
 	}
-
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("Hola desde la clase Seundaria");
+	}
 }
---
+++ 5.Spring Boot(Basico)/5.1.ComandLineRuner/ej5.1-command-line-runner/src/main/java/com/ej5/commandlinerunner/Init.java
@@ -0,0 +1,10 @@
+package com.ej5.commandlinerunner;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Init {
+    public void Message1(){
+        System.out.println("Hola desde clase Inicial");
+    }
+}
