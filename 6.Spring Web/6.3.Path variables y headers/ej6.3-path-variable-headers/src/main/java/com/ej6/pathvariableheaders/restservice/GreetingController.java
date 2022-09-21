273005fad0dbf3432691d39cf49960f735e4f7fd
juan-avila-salazar <juanpablo.avila@bosonit.com>
2022-09-21 17:50:15 +0200

    6.3

--- 6.Spring Web/6.3.Path variables y headers/ej6.3-path-variable-headers/src/main/java/com/ej6/pathvariableheaders/restservice/GreetingController.java
+++ 6.Spring Web/6.3.Path variables y headers/ej6.3-path-variable-headers/src/main/java/com/ej6/pathvariableheaders/restservice/GreetingController.java
@@ -1,19 +1,44 @@
 package com.ej6.pathvariableheaders.restservice;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
+@RequestMapping(value = "/greeting")
 public class GreetingController {
-
+    List<Greeting> listGreeting = new ArrayList<>();
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    @GetMapping(value = "/hello")
+    public Greeting hello(@RequestParam(value = "content", defaultValue = "World") String content) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, content));
     }
+    @PostMapping(value = "/addgreeting")
+    public Greeting addGreeting(@RequestBody Greeting greeting,
+                                @RequestParam(value = "content", defaultValue = "World") String content){
+        listGreeting.add(greeting);
+        return new Greeting(counter.incrementAndGet(), String.format(template, content));
+    }
+    @PutMapping (value = "/editgreeting")
+    public Greeting edit(@RequestParam(value = "content", defaultValue = "World")String content,
+                         @RequestParam long id) {
+        return new Greeting(id , content);
+    }
+    @GetMapping("/totallist")
+    public List<Greeting> totalList (){
+        listGreeting.stream().toList();
+        return listGreeting;
+    }
+    @GetMapping(value = "/search/{id}")
+    public List<Greeting> llamar(@PathVariable long id) {
+        List<Greeting> listId = listGreeting.stream().filter((ap)->ap.getId()==id).toList();
+        return listId;
+    }
+
+
+
 }
