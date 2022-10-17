66c6780648a390fe76cc15b258e9e7604e3b9e85
juan-avila-salazar <juanpablo.avila@bosonit.com>
2022-10-17 10:51:59 +0200

    examen 1

---
+++ 10.Docker/block10-dockerize-app/src/test/java/com/block10dockerize/app/Block10DockerizeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.block10dockerize.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Block10DockerizeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
