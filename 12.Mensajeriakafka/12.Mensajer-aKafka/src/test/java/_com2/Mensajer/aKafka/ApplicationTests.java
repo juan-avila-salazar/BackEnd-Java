e10db71049654b4b03b60d814a127fe23883c4b6
juan-avila-salazar <juanpablo.avila@bosonit.com>
2022-10-19 17:10:49 +0200

    ejercicio 12

---
+++ 12.Mensajeriakafka/12.Mensajer-aKafka/src/test/java/_com2/Mensajer/aKafka/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com2.Mensajer.aKafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
